4d12e45a0461e6f1e8e4574b888c1a1a01ba04b6
aziza <azizaagayeva220@gmail.com>
2026-07-05 11:12:30 +0400

    .

--- python/python ders/basic.py
+++ python/python ders/basic.py
@@ -1,12 +1,23 @@
 # print("hello world !!")
 
-x = 15 #int #intregent
-y = "5" # string sozler
+# x = 15 #int #intregent
+# y = "5" # string sozler
 
-print(x + 15)
+# # print(x + 15)
 
-x = input ("whats your name??")
-print(x)
+# # x = input ("whats your name??")
+# # print(x)
 
-year = int(input("tarixi yaz"))
-print(year)
+# year = int(input("tarixi yaz"))
+# print(year)
+
+# name = input("enter your name : ") 
+# age  = int(input("enter your age : "))
+# print("hello my name is ", name , "im", age ,)
+
+x = int(input("birinci reqem : "))
+y = int(input("ikinci reqem :"))
+
+print( " x + y ",( x + y ), "edir" )
+
+print( " x - y ",( x - y ), "edir" )
