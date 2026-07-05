e7fa3d5fcfa9ef3dbba343608e6f97d667f2aa68
aziza <azizaagayeva220@gmail.com>
2026-07-05 10:49:58 +0400

    .

--- python/python ders/basic.py
+++ python/python ders/basic.py
@@ -1,5 +1,12 @@
 # print("hello world !!")
 
 x = 15 #int #intregent
-y = 5
-print(x + y)
+y = "5" # string sozler
+
+print(x + 15)
+
+x = input ("whats your name??")
+print(x)
+
+year = int(input("tarixi yaz"))
+print(year)
