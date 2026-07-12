c3f95fa1ee817a52998b76f78f35030df082460d
aziza <azizaagayeva220@gmail.com>
2026-07-12 10:54:24 +0400

    .

---
+++ python/python ders/something.py
@@ -0,0 +1,11 @@
+# eded = int(input("eded daxil edin: "))
+
+# if eded % 2 == 0:
+#     print("cut")
+# else:
+#     print("tek")
+eded = int(input("bir eded daxil edin:"))
+# if (eded % 2 == 0):
+#     print("5 cixdi")
+# else:
+#     print("5 cixmadi")
